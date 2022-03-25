ee4390ff00d1ee9bf7e9ebb1bc51dba7675b6d9c
mdos-san <mdossan@protonmail.com>
2022-03-26 00:39:55 +0100

    feat: Improve TextField usability

--- src/components/TextField/index.tsx
+++ src/components/TextField/index.tsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 interface Props {
+  value: string;
+  onChange: (value: string) => void;
   type?: string;
   placeholder?: string;
 }
 
-export const TextField = ({type = "text", placeholder = ""}: Props) => {
-  return <input className="text-field" type={type} placeholder={placeholder}/>
-}
+export const TextField = ({
+  type = "text",
+  placeholder = "",
+  value,
+  onChange = () => {},
+}: Props) => {
+  return (
+    <input
+      className="text-field"
+      type={type}
+      placeholder={placeholder}
+      value={value}
+      onChange={(ev) => {
+        onChange(ev.currentTarget.value);
+      }}
+    />
+  );
+};
