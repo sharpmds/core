edc3fd8b953ddb02ccc54804aa249d64c3270134
mdos-san <mdossan@protonmail.com>
2022-03-27 00:36:41 +0100

    feat: Create round button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,15 +4,20 @@ interface Props {
   children: React.ReactNode;
   className?: string;
   onClick?: () => void;
+  round?: boolean;
 }
 
 export const Button = ({
   children,
   className = "",
+  round = false,
   onClick = () => {},
 }: Props) => {
   return (
-    <button className={`button ${className}`} onClick={onClick}>
+    <button
+      className={`button ${round ? "button--round" : ""} ${className}`}
+      onClick={onClick}
+    >
       {children}
     </button>
   );
